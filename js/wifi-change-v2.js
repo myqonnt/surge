eae2828007a2904fe3ab506ccccf07487e1a2b9d
weaving <onnt1997@outlook.com>
2025-04-20 21:13:04 +0800

    modify direct

---
+++ js/wifi-change-v2.js
@@ -0,0 +1,32 @@
+const OLD_WIFI_SSID_KEY = 'current_wifi_ssid';
+const CURRENT_WIFI_SSID = $network.wifi.ssid;
+if(!CURRENT_WIFI_SSID) {
+    // 如果没有连接wifi，直接返回
+    $done();
+}
+
+if (wifiChanged()) {
+    const args = $argument
+    // if args contains `,` convert to array
+    const WIFI_DONT_NEED_PROXY = args.includes(',') ? args.split(',') : [args];
+    const mode = WIFI_DONT_NEED_PROXY.includes(CURRENT_WIFI_SSID)
+        ? 'direct'
+        : 'rule';
+    $surge.setOutboundMode(mode);
+    $notification.post(
+        'Surge',
+        `Network changed to [${CURRENT_WIFI_SSID || 'cellular'}]`,
+        `use [${mode}] mode`
+    );
+}
+// 存储
+function wifiChanged() {
+    const changed = ($persistentStore.read(OLD_WIFI_SSID_KEY) !== CURRENT_WIFI_SSID);
+    changed && $persistentStore.write(CURRENT_WIFI_SSID, OLD_WIFI_SSID_KEY);
+    return changed;
+}
+
+$done();
+
+
+
